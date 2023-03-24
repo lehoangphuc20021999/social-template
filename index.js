127cb753ddd55bea58c091415a43dbf6824a64cb
lehoangphuc20021999 <lehoangphuc20021999@gmail.com>
2023-03-24 22:14:02 +0700

    Change npm start link

--- index.js
+++ index.js
@@ -1,109 +1,113 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const port = 'https://admirable-mochi-4b5a88.netlify.app/';
+const path = require('path');
+const bodyParser = require('body-parser');
 
 //---------------------------------------
-const passport = require('passport')
-const session = require('express-session')
+const passport = require('passport');
+const session = require('express-session');
 
 // Setting for Pugjs
 app.set('views', './views');
 app.set('view engine', 'pug');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Livereload
 var livereload = require('livereload');
 var watchServer = livereload.createServer();
 
 // Connect-livereload
-const connectLivereload = require("connect-livereload");
+const connectLivereload = require('connect-livereload');
 app.use(connectLivereload());
 watchServer.watch(path.join(__dirname, 'public'));
 
 // This is reset pug
-watchServer.server.once("connection", () => {
+watchServer.server.once('connection', () => {
   setTimeout(() => {
     watchServer.refresh('/');
   }, 500);
 });
 
 //- Luôn đặt nó ở vị trí này, do có hàm serializeUser, deserializeUser
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 // require('./db.js') //db
 //- KÍCH HOẠT GOOGLE STRATEGY
-app.get('/google',
+app.get(
+  '/google',
   passport.authenticate('google', {
-    scope:
-      ['email', 'profile']
-  }
-  ));
+    scope: ['email', 'profile'],
+  }),
+);
 //- NẾU FAIL THÌ TRỞ LẠI LOGIN (CÓ THỂ XÓA failureRedirect VÀ CUSTOM THÔNG QUA CALLBACK FUNCTION)
-app.get('/google/callback',
+app.get(
+  '/google/callback',
   passport.authenticate('google', {
     failureRedirect: '/login',
   }),
   function (req, res) {
     res.redirect('/');
-  });
+  },
+);
 
 //- REQUIRE PASSPORT
-require('./config/passport')
+require('./config/passport');
 
 //- IN RA THÔNG TIN USER KHI VỪA MỚI ĐĂNG NHẬP
 app.get('/', (req, res) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
 app.get('/phongban', (req, res) => {
-  res.render('department')
-})
+  res.render('department');
+});
 
 app.get('/thongbao', (req, res) => {
-  res.render('list-notification')
-})
+  res.render('list-notification');
+});
 
 app.get('/chitietthongbao', (req, res) => {
-  res.render('notification-details')
-})
+  res.render('notification-details');
+});
 
 app.get('/trangcanhan', (req, res) => {
-  res.render('personal')
-})
+  res.render('personal');
+});
 
 //- LOGIN, REGISTER
 app.get('/login', (req, res) => {
-  res.render('login')
-})
+  res.render('login');
+});
 
 app.get('/register', (req, res) => {
-  res.render('register')
-})
+  res.render('register');
+});
 
 //- MANAGE
 app.get('/quanlyphongban', (req, res) => {
-  res.render('manage-department')
-})
+  res.render('manage-department');
+});
 
 app.get('/quanlylop', (req, res) => {
-  res.render('manage-classes')
-})
+  res.render('manage-classes');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
